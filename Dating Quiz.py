80f5bf6af62211a35a1765d71afc0975864a4c15
fricorps <61331262+fricorps@users.noreply.github.com>
2020-07-23 22:00:34 -0700

    Add files via upload

---
+++ Dating Quiz.py
@@ -0,0 +1,16 @@
+
+
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print("Tall guy")
+elif not(is_male) and is_tall:
+    print("Spoon me please")
+elif is_male and not(is_tall):
+    print("Short guy")
+elif not(is_male) and not(is_tall):
+    print("Shawty")
+
+
+
